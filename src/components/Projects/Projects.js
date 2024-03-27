c429986c717a19d9b5636d2ae55e8519623a7a3d
gulamyazdani <gulamyazdani03@gmail.com>
2024-03-27 16:26:33 +0530

    Adding Project Section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -25,8 +25,8 @@ function Projects() {
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
+              title="Sample 1"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fermentum justo et nibh ullamcorper, eget rutrum nisi aliquet. Mauris euismod arcu ac sapien tincidunt, nec mollis nulla consectetur. Sed aliquam ex ac purus tincidunt, at convallis est finibus. Integer commodo tellus id elit efficitur, nec fermentum elit volutpat. Nullam nec purus nec odio bibendum posuere. Vestibulum vel sagittis velit. Nullam a lectus nec enim vehicula eleifend nec sed lorem."
               ghLink="/"
               demoLink="/"
             />
@@ -36,8 +36,8 @@ function Projects() {
             <ProjectCard
               imgPath={bitsOfCode}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
+              title="Sample 2"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fermentum justo et nibh ullamcorper, eget rutrum nisi aliquet. Mauris euismod arcu ac sapien tincidunt, nec mollis nulla consectetur. Sed aliquam ex ac purus tincidunt, at convallis est finibus. Integer commodo tellus id elit efficitur, nec fermentum elit volutpat. Nullam nec purus nec odio bibendum posuere. Vestibulum vel sagittis velit. Nullam a lectus nec enim vehicula eleifend nec sed lorem."
               ghLink="/"
               demoLink="/"
             />
@@ -47,8 +47,8 @@ function Projects() {
             <ProjectCard
               imgPath={editor}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
+              title="Sample 3"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fermentum justo et nibh ullamcorper, eget rutrum nisi aliquet. Mauris euismod arcu ac sapien tincidunt, nec mollis nulla consectetur. Sed aliquam ex ac purus tincidunt, at convallis est finibus. Integer commodo tellus id elit efficitur, nec fermentum elit volutpat. Nullam nec purus nec odio bibendum posuere. Vestibulum vel sagittis velit. Nullam a lectus nec enim vehicula eleifend nec sed lorem."
               ghLink="/"
               demoLink="/"              
             />
@@ -58,8 +58,8 @@ function Projects() {
             <ProjectCard
               imgPath={leaf}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
+              title="Sample 4"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fermentum justo et nibh ullamcorper, eget rutrum nisi aliquet. Mauris euismod arcu ac sapien tincidunt, nec mollis nulla consectetur. Sed aliquam ex ac purus tincidunt, at convallis est finibus. Integer commodo tellus id elit efficitur, nec fermentum elit volutpat. Nullam nec purus nec odio bibendum posuere. Vestibulum vel sagittis velit. Nullam a lectus nec enim vehicula eleifend nec sed lorem."
               ghLink="/"
               demoLink="/"
             />
@@ -69,8 +69,8 @@ function Projects() {
             <ProjectCard
               imgPath={suicide}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
+              title="Sample 5"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fermentum justo et nibh ullamcorper, eget rutrum nisi aliquet. Mauris euismod arcu ac sapien tincidunt, nec mollis nulla consectetur. Sed aliquam ex ac purus tincidunt, at convallis est finibus. Integer commodo tellus id elit efficitur, nec fermentum elit volutpat. Nullam nec purus nec odio bibendum posuere. Vestibulum vel sagittis velit. Nullam a lectus nec enim vehicula eleifend nec sed lorem."
               ghLink="/"
               demoLink="/"
             />
@@ -80,9 +80,8 @@ function Projects() {
             <ProjectCard
               imgPath={emotion}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
+              title="Sample 6"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fermentum justo et nibh ullamcorper, eget rutrum nisi aliquet. Mauris euismod arcu ac sapien tincidunt, nec mollis nulla consectetur. Sed aliquam ex ac purus tincidunt, at convallis est finibus. Integer commodo tellus id elit efficitur, nec fermentum elit volutpat. Nullam nec purus nec odio bibendum posuere. Vestibulum vel sagittis velit. Nullam a lectus nec enim vehicula eleifend nec sed lorem."
               ghLink="/"
               demoLink="/" 
             />
